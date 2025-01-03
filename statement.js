fd0a7bc41482a15c56301f680221b59297831c87
Mike Thomas <mike.george.thomas@gmail.com>
2025-01-03 15:01:20 +0000

    Replace loop with pipeline: totalAmount and totalVolumeCredits

--- statement.js
+++ statement.js
@@ -40,19 +40,13 @@ function statement (invoice, plays) {
     return result;
   }
   function totalAmount(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
+    return data.performances
+      .reduce((total, p) => total + p.amount, 0);
   }
 
   function totalVolumeCredits(data) {
-    let volumeCredits = 0;
-    for (let perf of data.performances) {
-      volumeCredits += perf.volumeCredits;
-    }
-    return volumeCredits;
+    return data.performances
+      .reduce((total, p) => total + p.volumeCredits, 0);
   }
 
   function volumeCreditsFor(aPerformance) {
